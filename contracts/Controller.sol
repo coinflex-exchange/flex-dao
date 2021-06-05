dba2c7986c9f61f2cea2e8784080f1e3da439847
Sitt Guruvanich <aekasitt@gmail.com>
2021-06-05 23:47:35 +0700

    Removed ref to IVaultConverter

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -6,7 +6,6 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '../interfaces/IVault.sol';
-// import '../interfaces/IVaultConverter.sol';
 import '../interfaces/IOneSplitAudit.sol';
 import '../interfaces/IStrategy.sol';
 import '../interfaces/IConverter.sol';
