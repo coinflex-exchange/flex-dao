63e8478881d14b9bcff5aea5498797a6d56432f0
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-14 05:28:53 +0700

    Removed voting escrow

--- interfaces/IVotingEscrow.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IVotingEscrow
-{
-  function deposit_for(address, uint256) external;
-  function balanceOf(address addr, uint256 timestamp) external view returns(uint256);
-  function totalSupply(uint256 timestamp) external view returns(uint256);
-}
