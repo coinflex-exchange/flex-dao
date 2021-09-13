c47f88d672d7b929acb40408bca5ad3f1e0c5403
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-13 13:20:14 +0700

    Refactored docstring; Create fixture

--- tests/timelock.py
+++ tests/timelock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 # coding:utf-8
 # Copyright (C) 2019-2021 All rights reserved.
-# FILENAME:  tests/__init__.py
+# FILENAME:  tests/timelock.py
 # VERSION: 	 1.0
 # CREATED: 	 2021-09-03 14:07
 # AUTHOR: 	 Aekasitt Guruvanich <sitt@coinflex.com>
@@ -17,6 +17,20 @@ from eth_account import Account
 from pytest import mark, raises
 ### Local Modules ###
 from . import *
+
+@fixture
+def deploy_timelock(admin: Account, gas_price: dict) -> Timelock:
+  '''
+  FIXTURE: Deploy a Timelock contract for 14 days to be used by other contracts' testing.
+
+  ---
+  :param: admin  `Account`  the wallet address to deploy the contract from
+  :param: gas_price  `dict`  the mock gas_price object as it would be like to receive from Gas Station API  
+  :returns: `Timelock`  
+  '''
+  delay: int     = 14 * 24 * 60 * 60
+  gas_speed: str = 'fast'
+  return Timelock.deploy(admin, delay, {'from': admin, 'gas_limit': gas_price[gas_speed] })
 
 @mark.parametrize('delay, gas_speed', (
   (2  * 24 * 60 * 60, 'fast'),     # 2 days
