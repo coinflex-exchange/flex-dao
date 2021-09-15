475d9faff1d3950de32dc0ad56f2b65019b194ab
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-16 01:17:32 +0700

    removed unused interfaces

--- interfaces/IDistributor.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IDistributor {
-  function deposit(uint, uint) external;
-  function withdraw(uint, uint) external;
-  function userInfo(uint, address) external view returns(uint, uint);
-}
