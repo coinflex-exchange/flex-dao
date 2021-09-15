b585c102c115d51df3e52080c7b0dee991b10fbd
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-15 22:45:42 +0700

    Changed module FILENAME in comment

--- tests/staking_rewards.py
+++ tests/staking_rewards.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 # coding:utf-8
 # Copyright (C) 2019-2021 All rights reserved.
-# FILENAME:  tests/__init__.py
+# FILENAME:  tests/staking_rewards.py
 # VERSION: 	 1.0
 # CREATED: 	 2021-09-03 14:07
 # AUTHOR: 	 Aekasitt Guruvanich <sitt@coinflex.com>
