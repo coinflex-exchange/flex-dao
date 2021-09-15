c48a030ac04e281a900db868f073a28045e5e865
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-15 14:22:33 +0700

    Removed unused interface

--- interfaces/IStakingRewards.sol
+++ interfaces/IStakingRewards.sol
@@ -26,17 +26,3 @@ interface IStakingRewards {
   function userRewardPerTokenPaid(address) external view returns(uint256);
   function withdraw(uint256 amount) external;
 }
-
-interface IStakingRewardsFactory {
-  function deploy(address stakingToken, uint256 rewardAmount) external;
-  function isOwner() external view returns(bool);
-  function notifyRewardAmount(address stakingToken) external;
-  function notifyRewardAmounts() external;
-  function owner() external view returns(address);
-  function renounceOwnership() external;
-  function rewardsToken() external view returns(address);
-  function stakingRewardsGenesis() external view returns(uint256);
-  function stakingRewardsInfoByStakingToken(address) external view returns(address stakingRewards, uint256 rewardAmount);
-  function stakingTokens(uint256) external view returns(address);
-  function transferOwnership(address newOwner) external;
-}
