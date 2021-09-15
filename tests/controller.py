9e01caa3aa5f90bdcdeb0fb7dc5dbd4bd2fc9a6f
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-15 22:36:26 +0700

    Refactored top doc

--- tests/controller.py
+++ tests/controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 # coding:utf-8
 # Copyright (C) 2019-2021 All rights reserved.
-# FILENAME:  tests/daotoken.py
+# FILENAME:  tests/controller.py
 # VERSION: 	 1.0
 # CREATED: 	 2021-09-12 09:51
 # AUTHOR: 	 Aekasitt Guruvanich <sitt@coinflex.com>
