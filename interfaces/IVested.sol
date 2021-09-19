cdc32cb74dd4e5f954303e02cf42a583bb92f760
Aekasitt Guruvanich <aekazitt@gmail.com>
2021-09-19 12:55:11 +0700

    [Fix] refactored IVault to IVested

--- interfaces/IVested.sol
+++ interfaces/IVested.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IVault {
+interface IVested {
   function deposit_for(address, uint256) external;
   function balanceOf(address addr, uint256 timestamp) external view returns(uint256);
   function totalSupply(uint256 timestamp) external view returns(uint256);
