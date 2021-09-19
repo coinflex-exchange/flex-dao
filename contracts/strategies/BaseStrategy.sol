b7113d0590fa00b9f96bf2fbc2d3485cd191ed43
Aekasitt Guruvanich <aekazitt@gmail.com>
2021-09-19 12:57:57 +0700

    [Fix] Removed unused interfaces

--- contracts/strategies/BaseStrategy.sol
+++ contracts/strategies/BaseStrategy.sol
@@ -6,8 +6,6 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
-import '../../interfaces/IVault.sol';
-import '../../interfaces/IDistributor.sol';
 import '../../interfaces/IController.sol';
 
 // Strategy Contract Basics
