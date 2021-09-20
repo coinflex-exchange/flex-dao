dab03c3e946a54ee4862d1bd3ffc4a4268e6df2a
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-20 22:40:31 +0700

    Renamed RewardShare to RevenueShare

--- contracts/RevenueShare.sol
+++ contracts/RevenueShare.sol
@@ -7,7 +7,7 @@ import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '../interfaces/IVested.sol';
 
-contract RewardShare is Ownable
+contract RevenueShare is Ownable
 {
   using SafeERC20 for IERC20;
   using SafeMath for uint256;
