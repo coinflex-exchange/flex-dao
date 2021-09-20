f2dec083f3481252a0eca76d6dfb9ede9eff3424
Sitt Guruvanich <aekazitt@gmail.com>
2021-09-20 22:51:40 +0700

    Refactored fVault to using veFLEX instead

--- tests/staking_rewards.py
+++ tests/staking_rewards.py
@@ -10,47 +10,45 @@
 # HISTORY:
 #*************************************************************
 ### Project Contracts ###
-from brownie import FLEXCoin, fVault, StakingRewards
+from brownie import FLEXCoin, veFLEX, StakingRewards
 ### Third-Party Packages ###
 from brownie.network.gas.strategies import GasNowStrategy
 from eth_account import Account
 from pytest import fixture, mark
 ### Local Modules ###
 from . import *
-from .controller import deploy_controller # used by deploy_ve_flex
 from .flex import deploy_flex
 from .ve_flex import deploy_ve_flex
-from .timelock import deploy_timelock # used by deploy_ve_flex
 
 @fixture
-def deploy_staking_rewards(admin: Account, deploy_flex: FLEXCoin, deploy_ve_flex: fVault) -> StakingRewards:
+def deploy_staking_rewards(admin: Account, deploy_flex: FLEXCoin, deploy_ve_flex: veFLEX) -> StakingRewards:
   '''
   FIXTURE: Returns a StakingRewards contract given flex and RewardToken to be used by other contracts' testing.  
 
   ---
   :param: admin  `Account`  the wallet address to deploy the contract from  
   :param: deploy_flex  `FLEXCoin`  generic ERC-20 to serve as the Staking Token  
-  :param: deploy_ve_flex  `fVault`  ERC-20 compliant Reward Token to be given out as rewards  
+  :param: deploy_ve_flex  `veFLEX`  ERC-20 compliant Reward Token to be given out as rewards  
   :returns:  `StakingRewards`  
   '''
   flex: FLEXCoin  = deploy_flex
-  ve_flex: fVault = deploy_ve_flex
+  ve_flex: veFLEX = deploy_ve_flex
   gas_strategy    = GasNowStrategy('fast')
   return StakingRewards.deploy(ve_flex, flex, { 'from': admin, 'gas_price': gas_strategy })
 
 @mark.parametrize('gas_speed', ('fast', 'standard'))
-def test_deploy_staking_rewards(admin: Account,  deploy_flex: FLEXCoin, deploy_ve_flex: fVault, gas_speed: str):
+def test_deploy_staking_rewards(admin: Account,  deploy_flex: FLEXCoin, deploy_ve_flex: veFLEX, gas_speed: str):
   '''
   TEST: Deploy a StakingRewards contract given flex and RewardToken
 
   ---
   :param: admin  `Account`  the wallet address to deploy the contract from  
   :param: deploy_flex  `FLEXCoin`  generic ERC-20 to serve as the Staking Token  
-  :param: deploy_ve_flex  `fVault`  ERC-20 compliant Reward Token to be given out as rewards  
+  :param: deploy_ve_flex  `veFLEX`  ERC-20 compliant Reward Token to be given out as rewards  
   :param: gas_speed  `str`  the mock speed key to be used with gas_price object; either `fast` or `standard`  
   '''
   flex: FLEXCoin  = deploy_flex
-  ve_flex: fVault = deploy_ve_flex
+  ve_flex: veFLEX = deploy_ve_flex
   gas_strategy    = GasNowStrategy(gas_speed)
   ### Deployment ###
   staking_rewards: StakingRewards = StakingRewards.deploy(ve_flex, flex, { 'from': admin, 'gas_price': gas_strategy })
--- tests/ve_flex.py
+++ tests/ve_flex.py
@@ -10,58 +10,47 @@
 # HISTORY:
 #*************************************************************
 ### Project Contracts ###
-from brownie import Controller, FLEXCoin, fVault, Timelock
+from brownie import Controller, FLEXCoin, veFLEX, Timelock
 ### Third-Party Packages ###
 from brownie.network.gas.strategies import GasNowStrategy
 from eth_account import Account
 from pytest import mark
 ### Local Modules ###
 from . import *
-from .controller import deploy_controller
 from .flex import deploy_flex
-from .timelock import deploy_timelock
 
 @fixture
-def deploy_ve_flex(admin: Account, governance: Account, \
-  deploy_controller: Controller, deploy_flex: FLEXCoin, deploy_timelock: Timelock) -> fVault:
+def deploy_ve_flex(admin: Account, deploy_flex: FLEXCoin) -> veFLEX:
   '''
-  FIXTURE: Returns deployed fVault contract to be used by other contract testing
+  FIXTURE: Returns deployed veFLEX contract to be used by other contract testing
 
   ---
   :param: admin  `Account`  the wallet address to deploy the contract from  
   :param: governance  `Account`  the wallet address defined as DAO Controller governance  
-  :param: deploy_controller  `Controller`  the main contract of the DAO  
   :param: deploy_flex  `FLEXCoin`  generic ERC-20 to serve as the Staking Token; Preferably FLEX  
-  :param: deploy_timelock  `Timelock`  
-  :returns: `fVault`
+  :returns: `veFLEX`
   '''
-  controller: Controller = deploy_controller
   flex: FLEXCoin         = deploy_flex
-  timelock: Timelock     = deploy_timelock
   gas_strategy           = GasNowStrategy('fast')
+  version: str           = 'v1'
   ### Deployment ###
-  return fVault.deploy(flex, governance, timelock, controller, { 'from': admin, 'gas_price': gas_strategy })
+  return veFLEX.deploy(flex, f'vested {flex.name()}', f've{flex.symbol()}', version, { 'from': admin, 'gas_price': gas_strategy })
 
-@mark.parametrize('gas_speed', ('fast', 'standard'))
-def test_deploy_ve_flex(admin: Account, governance: Account, \
-  deploy_controller: Controller, deploy_flex: FLEXCoin, deploy_timelock: Timelock, gas_speed: str):
+@mark.parametrize('gas_speed, version', [('fast', 'v0'), ('standard', 'v1')])
+def test_deploy_ve_flex(admin: Account, governance: Account, deploy_flex: FLEXCoin, gas_speed: str, version: str):
   '''
-  TEST: Deploy fVault Contract
+  TEST: Deploy veFLEX Contract
   
   ---
   :param: admin  `Account`  the wallet address to deploy the contract from  
   :param: governance  `Account`  the wallet address defined as DAO Controller governance  
-  :param: deploy_controller  `Controller`  the main contract of the DAO  
   :param: deploy_flex  `FLEXCoin`  generic ERC-20 to serve as the Staking Token; Preferably FLEX  
-  :param: deploy_timelock  `Timelock`  
   :param: gas_speed  `str`  the mock speed key to be used with gas_price object; either `fast` or `standard`  
   '''
-  controller: Controller = deploy_controller
   flex: FLEXCoin         = deploy_flex
-  timelock: Timelock     = deploy_timelock
   gas_strategy           = GasNowStrategy(gas_speed)
   ### Deployment ###
-  ve_flex        = fVault.deploy(flex, governance, timelock, controller, { 'from': admin, 'gas_price': gas_strategy })
-  assert ve_flex.name()   == f'flex { flex.name() }'
-  assert ve_flex.symbol() == f'f{ flex.symbol() }'
+  ve_flex        = veFLEX.deploy(flex, f'vested {flex.name()}', f've{flex.symbol()}', 'v1', { 'from': admin, 'gas_price': gas_strategy })
+  assert ve_flex.name()   == f'vested { flex.name() }'
+  assert ve_flex.symbol() == f've{ flex.symbol() }'
   print(f'veFLEX: { ve_flex }')
