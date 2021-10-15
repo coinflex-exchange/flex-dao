4a1e8b1214585378bfc8d4fcf4ec6948c7d25ff0
Sitt Guruvanich <aekazitt@gmail.com>
2021-10-15 12:44:46 +0700

    Tests distributions and claims

--- tests/distributions/daily.py
+++ tests/distributions/daily.py
@@ -27,7 +27,7 @@ from tests.deployments.ve_flex import deploy_ve_flex # Used by deploy_daily_payo
 
 @mark.parametrize('gas_speed', ('standard', 'fast'))
 def test_distribute_calls(admin: Account, deploy_flex: FLEXCoin, deploy_daily_payout: DailyPayout, gas_speed: str):
-  epochs: int           = 1
+  epochs: int           = 10
   flex: FLEXCoin        = deploy_flex
   payout: DailyPayout   = deploy_daily_payout
   flex_balance: Decimal = flex.balanceOf(admin)
@@ -40,25 +40,32 @@ def test_distribute_calls(admin: Account, deploy_flex: FLEXCoin, deploy_daily_pa
 
 @mark.parametrize('gas_speed', ('standard', 'fast'))
 def test_claim_calls(admin: Account, user_accounts: List[Account], deploy_flex: FLEXCoin, deploy_ve_flex: veFLEX, deploy_daily_payout: DailyPayout, gas_speed: str):
-  epochs: int           = 5
+  epochs: int           = 50
   flex: FLEXCoin        = deploy_flex
   payout: DailyPayout   = deploy_daily_payout
   ve_flex: veFLEX       = deploy_ve_flex
   flex_balance: Decimal = flex.balanceOf(admin)
-  payout_per_epoch      = flex_balance / 2 / epochs
+  payout_per_epoch      = flex_balance / (epochs * 2)
   gas_strategy          = GasNowStrategy(gas_speed)
   ### Execute ###
   for _ in range(epochs):
     payout.distribute(payout_per_epoch, { 'from': admin, 'gas_speed': gas_strategy })
     print(f'Current Epoch: { payout.currentEpoch() }')
   ### Vest ###
+  chain: Chain = Chain() # get chain instance  
   claimant     = user_accounts[0]
   flex.transfer(claimant, flex.balanceOf(admin), { 'from': admin, 'gas_speed': gas_strategy })
-  chain: Chain = Chain() # get chain instance
-  unlock_time  = chain.time() + 4 * 365 * 86400 # 1 week in seconds
+  unlock_time  = chain.time() + (4 * 365 * 86400) # 4 years in seconds
   ve_flex.create_lock(flex.balanceOf(claimant), unlock_time, { 'from': claimant, 'gas_price': gas_strategy })
   print(f'veFLEX Balance: { ve_flex.balanceOf(claimant) }')
+  ### Sets Payout StartTime ###
+  payout.setStartTime(chain.time(), { 'from': admin, 'gas_price': gas_strategy })
+  ### Sleep ###
+  chain.sleep(86400) # 1 day in seconds
   ### Claim ###
   for _ in range(epochs):
     claimable: Decimal = payout.getClaimable(claimant)#, { 'from': claimant, 'gas_price': gas_strategy })
-    print(claimable)
+    print(f'Claimable Amount: { claimable }')
+    claim_txn = payout.claim(claimant, { 'from': admin })
+    print(f'Claim Txn: { claim_txn }')
+    chain.sleep(86400) # 1 day in seconds
