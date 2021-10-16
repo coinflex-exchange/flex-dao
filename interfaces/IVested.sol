85098bad4a34e4a3bee31a366dbaf48828ea85db
Samuel-Isaias <sam.isa@coinflex.com>
2021-10-16 08:07:13 +0000

    added signatures of balanceOfAt and totalSupplyAt

--- interfaces/IVested.sol
+++ interfaces/IVested.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 interface IVested {
   function deposit_for(address, uint256) external;
   function balanceOf(address addr, uint256 timestamp) external view returns(uint256);
+  function balanceOfAt(address addr, uint256 _block) external view returns(uint256);
   function totalSupply(uint256 timestamp) external view returns(uint256);
+  function totalSupplyAt(uint256 _block) external view returns(uint256);
 }
