76f564c95d43fbd6d8ff91137e9750af2ef9410e
Tang Wei <wei.tang@coinflex.com>
2021-12-16 18:26:53 +0800

    added script to export abi of all contracts

---
+++ scripts/tools/exportAllAbi.py
@@ -0,0 +1,16 @@
+import json
+import os
+import glob
+import pprint
+keywordList = []
+
+path = 'build/contracts/'
+outputDir = 'build/abi/'
+for filename in glob.glob(os.path.join(path, '*.json')):
+  print(f'Read from      {filename}')
+  with open(filename) as currentFile:
+    abi = json.load(currentFile)["abi"]
+    save_path = os.path.join(outputDir, filename.split('/')[-1])
+    with open(save_path, 'w') as outfile:
+      json.dump(abi, outfile)
+      print(f'Save ABI into: {save_path}\n')
