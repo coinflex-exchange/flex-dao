ef0200552c8fa651a7d3e2757563d20304dcb8a8
Samuel Isaias <sam.isa@coinflex.com>
2022-03-04 10:06:27 +0800

    removed unused function '_execute'

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -132,24 +132,4 @@ contract Controller
     IStrategy(strategies[_token]).withdraw(_amount);
   }
 
-  function _execute(address _target, bytes memory _data)
-    internal
-    returns (bytes memory response)
-  {
-    require(_target != address(0), '!target');
-    // call contract in current context
-    assembly {
-      let succeeded := delegatecall(sub(gas(), 5000), _target, add(_data, 0x20), mload(_data), 0, 0)
-      let size := returndatasize()
-      response := mload(0x40)
-      mstore(0x40, add(response, and(add(add(size, 0x20), 0x1f), not(0x1f))))
-      mstore(response, size)
-      returndatacopy(add(response, 0x20), 0, size)
-      switch iszero(succeeded)
-        case 1 {
-          // throw if delegatecall failed
-          revert(add(response, 0x20), size)
-        }
-    }
-  }
 }
